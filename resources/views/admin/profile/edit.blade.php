50489ff87a44a9bffd709b37df190bba3615afd1
nanaokabe <okabe.nana@icloud.com>
2021-08-11 05:33:53 +0000

    editcreate

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCYPE html>
+<html>
+  <head>
+  <meta chaset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="view-port" content="width=device-width, iniical-scale=1">
+  
+  <title>Edit</title>
+  </head>
+    <body>
+      <h1>Edit</h1>
+    </body>
+    
+</html>
