fcf9bf5ad64aebaf9aa1e62a0a4600579e72394e
nanaokabe <okabe.nana@icloud.com>
2021-09-03 06:02:37 +0000

    Herokuへの画像アップロード

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -11,11 +11,13 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="caption mx-auto">
+                              
                                 <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
+                                @if ($headline->image_path)
+                               <img src="{{ $headline->image_path }}">
+                                @endif
                                 </div>
+                                
                                 <div class="title p-2">
                                     <h1>{{ str_limit($headline->title, 70) }}</h1>
                                 </div>
@@ -45,11 +47,13 @@
                                     {{ str_limit($post->body, 1500) }}
                                 </div>
                             </div>
+                            
                             <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
+                            @if ($post->image_path)
+                           <img src="{{ $post->image_path }}">
+                            @endif
                             </div>
+                            
                         </div>
                     </div>
                     <hr color="#c0c0c0">
